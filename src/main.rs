c690cc655f95d46b9476014238e46df45b0d225f
JasonWang <1103870790@qq.com>
2020-04-03 00:14:14 +0800

    add eqtb

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
 use std::process;
 use std::collections::HashMap;
 use std::char;
+use std::fs::File;
+use std::marker::Copy;
+use std::ops::{Index, IndexMut};
+use byteorder::{ByteOrder, LittleEndian};
 
+
+/* 
+ * Here are types of TeX
+ */
+struct Array<T:Default+Clone> {
+    min: usize,
+    max: usize,
+    data: Vec<T>,
+}
+
+impl<T:Default+Clone> Array<T> {
+    pub fn new(min:usize, max:usize) -> Array<T> {
+        Array {
+            min: min,
+            max: max,
+            data: vec![Default::default(); max - min + 1],
+        }
+    }
+}
+
+impl<T:Default+Clone> Index<usize> for Array<T> {
+    type Output = T;
+
+    fn index(&self, i: usize) -> &Self::Output {
+        return &self.data[self.min + i];
+    }
+}
+
+impl<T:Default+Clone> IndexMut<usize> for Array<T> {
+    fn index_mut(&mut self, i: usize) -> &mut Self::Output {
+        return &mut self.data[self.min + i];
+    }
+}
 
 // 73
 pub enum Interaction
@@ -23,26 +60,148 @@ impl Interaction {
     }
 }
 
-pub enum History
-{
-    Spotless,
-    WarningIssued,
-    ErrorMessageIssued,
-    FatalErrorStop,
+// 101
+const unity: i32 = 0200000;
+const two: i32 = 0400000;
+type Scaled = i32;
+type NonnegativeInteger = i32;
+type SmallNumber = i32;
+
+
+// 109
+type GlueRatio = f32;
+
+// 113
+type QuarterWord = u8;  // 0..255
+type HalfWord = u16;    // 0..65535
+type TwoChoices = u8;   // 1..2
+type FourChoices = u8;  // 1..4
+
+#[derive(Debug, Clone, Copy)]
+enum TwoHalvesLeftHalf {
+    OneHalfWord(HalfWord),
+    TwoQuarterWord(QuarterWord, QuarterWord),
 }
 
+#[derive(Debug, Clone, Copy)]
+struct TwoHalves {
+    data: [u8; 4],
+}
 
-impl History {
-    fn value(&self) -> i32 {
-        match self {
-            History::Spotless => 0,
-            History::WarningIssued => 1,
-            History::ErrorMessageIssued => 2,
-            History::FatalErrorStop => 3,
+impl TwoHalves {
+    pub fn new() -> TwoHalves {
+        TwoHalves {
+            data: [0; 4]
+        }
+    }
+
+    pub fn rh(&self) -> HalfWord {
+        LittleEndian::read_u16(&self.data[2..4])
+    }
+
+    pub fn set_rh(&mut self, value: HalfWord) {
+        LittleEndian::write_u16(&mut self.data[2..4], value);
+    }
+
+    pub fn lh(&self) -> HalfWord {
+        LittleEndian::read_u16(&self.data[0..2])
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct FourQuarters {
+	b0: QuarterWord,
+	b1: QuarterWord,
+	b2: QuarterWord,
+	b3: QuarterWord,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct MemoryWord {
+    data: [u8; 4],
+}
+
+impl MemoryWord {
+    pub fn new() -> MemoryWord {
+        Default::default()
+    }
+
+    pub fn new_i32(value: i32) -> MemoryWord {
+        let mut ret: MemoryWord = Default::default();
+        LittleEndian::write_i32(&mut ret.data, value);
+        ret
+    }
+
+    pub fn int(&self) -> i32 {
+        LittleEndian::read_i32(&self.data)
+    }
+
+    pub fn gr(&self) -> GlueRatio {
+        LittleEndian::read_f32(&self.data)
+    }
+
+    pub fn hh_rh(&self) -> u16 {
+        LittleEndian::read_u16(&self.data[2..4])
+    }
+
+    pub fn set_hh_rh(&mut self, value: u16) {
+        LittleEndian::write_u16(&mut self.data[2..4], value);
+    }
+
+    pub fn hh_lh(&self) -> u16 {
+        LittleEndian::read_u16(&self.data[0..2])
+    }
+
+    pub fn set_hh_lh(&mut self, value: u16) {
+        LittleEndian::write_u16(&mut self.data[0..2], value);
+    }
+
+    pub fn qqqq(&self) -> FourQuarters {
+        FourQuarters {
+            b0: self.data[0],
+            b1: self.data[1],
+            b2: self.data[2],
+            b3: self.data[3],
         }
     }
 }
 
+impl Default for MemoryWord {
+    fn default() -> Self {
+        MemoryWord {
+            data: [0; 4]
+        }
+    }
+}
+
+type WordFile = File;
+
+
+// 212
+#[derive(Debug, Clone, Default, Copy)]
+struct ListStateRecord {
+    mode_field: i32,
+    head_field: HalfWord,
+    tail_field: HalfWord,
+    pg_field: i32,
+    ml_field: i32,
+    aux_field: MemoryWord,
+}
+
+impl ListStateRecord {
+    pub fn new() -> ListStateRecord {
+        ListStateRecord {
+            mode_field: 0,
+            head_field: 0,
+            tail_field: 0,
+            pg_field: 0,
+            ml_field: 0,
+            aux_field: Default::default(),
+        }
+    }
+}
+
+// 222
 enum EquivRegion2Code
 {
 	ActiveBase,
@@ -67,13 +226,13 @@ enum EquivRegion2Code
 }
 
 impl EquivRegion2Code {
-    fn value(&self) -> i16 {
+    fn value(&self) -> HalfWord {
         match self {
             ActiveBase => 1,
             SingleBase => EquivRegion2Code::ActiveBase.value() + 256,
             NullCs => EquivRegion2Code::SingleBase.value() + 256,
             HashBase => EquivRegion2Code::NullCs.value() + 1,
-            FrozenControlSequence => EquivRegion2Code::HashBase.value() + TexState::hash_size as i16,
+            FrozenControlSequence => EquivRegion2Code::HashBase.value() + TexState::hash_size as HalfWord,
             FrozenProtection => EquivRegion2Code::FrozenControlSequence.value(),
             FrozenCr => EquivRegion2Code::FrozenControlSequence.value() + 1,
             FrozenEndGroup => EquivRegion2Code::FrozenControlSequence.value() + 2,
@@ -85,12 +244,153 @@ impl EquivRegion2Code {
             EndWrite => EquivRegion2Code::FrozenControlSequence.value() + 8,
             FrozenDontExpand => EquivRegion2Code::FrozenControlSequence.value() + 9,
             FrozenNullFont => EquivRegion2Code::FrozenControlSequence.value() + 10,
-            FontIdBase => EquivRegion2Code::FrozenNullFont.value() - TexState::font_base as i16,
+            FontIdBase => EquivRegion2Code::FrozenNullFont.value() - TexState::font_base as HalfWord,
             UndefinedControlSequence => EquivRegion2Code::FrozenNullFont.value() + 257,
             GlueBase => EquivRegion2Code::UndefinedControlSequence.value() + 1,
         }
     }
 }
+
+// Region 3 of eqtb
+// 224
+enum EquivRegion3Code
+{
+	LineSkipCode, // interline glue if baseline_skip is infeasible
+	BaselineSkipCode, // desired glue between baselines
+	ParSkipCode, // extra glue just above a paragraph
+	AboveDisplaySkipCode, // extra glue just above displayed math
+	BelowDisplaySkipCode, // extra glue just below deisplayed math
+	AboveDisplayShortSkipCode, // glue above displayed math following short lines
+	BelowDisplayShortSkipCode,  // glue below displayed math following short lines
+	LeftSkipCode, // glue at left of justified lines
+	RightSkipCode, // glue at right of justified lines
+	TopSkipCode, // glue at top of main pages
+	SplitTopSkipCode, // glue at top of split pages
+	TabSkipCode, // glue between aligned entries
+	SpaceSkipCode, // glue between words (if not zero_glue)
+	XspaceSkipCode, // glue after sentences (if not zero glue)
+	ParFillSkipCode, // glue on last line of paragraph
+	ThinMuSkipCode, // thin space in math formula
+	MedMuSkipCode, // medium space in math formula
+	ThickMuSkipCode, // thick space in math formula
+}
+
+
+impl EquivRegion3Code {
+    fn value(&self) -> HalfWord {
+        match self {
+            LineSkipCode => 0,
+            BaselineSkipCode => 1,
+            ParSkipCode => 2,
+            AboveDisplaySkipCode => 3,
+            BelowDisplaySkipCode => 4,
+            AboveDisplayShortSkipCode => 5,
+            BelowDisplayShortSkipCode => 6,
+            LeftSkipCode => 7,
+            RightSkipCode => 8,
+            TopSkipCode => 9,
+            SplitTopSkipCode => 10,
+            TabSkipCode => 11,
+            SpaceSkipCode => 12,
+            XspaceSkipCode => 13,
+            ParFillSkipCode => 14,
+            ThinMuSkipCode => 15,
+            MedMuSkipCode => 16,
+            ThickMuSkipCode => 17,
+        }
+    }
+}
+
+
+const glue_pars: usize = 18; // total number of glue parameters
+const skip_base: usize = glue_base + glue_pars; // table of 256 ``skip'' registers
+const mu_skip_base: usize = skip_base + 256; // table of 256 ``muskip'' registers
+const local_base: usize = mu_skip_base + 256; // beginning of region 4
+
+
+// region 4 of eqtb
+// 230
+enum EquivRegion4Loc
+{
+	ParShapeLoc, // specifies paragraph shape
+	OutputRoutineLoc, // points to token list for \output
+	EveryParLoc, // points to token list for \everypar
+	EveryMathLoc, // points to token list for \everymath
+	EveryDisplayLoc, // points to token list for \everydisplay
+	EveryHboxLoc, // points to token list for \everyhbox
+	EveryVboxLoc, // points to token list for \everyvbox
+	EveryJobLoc, // points to token list for \everyjob
+	EveryCrLoc, // points to token list for \everycr
+	ErrHelpLoc, // points to token list for \errhelp
+	ToksBase, // table of 256 token list registers
+	BoxBase, // table of 256 box registers
+	CurFontLoc, // internal font number outside math mode
+}
+
+
+impl EquivRegion4Loc {
+    fn value(&self) -> HalfWord {
+        match self {
+            ParShapeLoc => local_base as HalfWord,
+            OutputRoutineLoc => local_base as HalfWord + 1,
+            EveryParLoc => local_base as HalfWord + 2,
+            EveryMathLoc => local_base as HalfWord + 3,
+            EveryDisplayLoc => local_base as HalfWord + 4,
+            EveryHboxLoc => local_base as HalfWord + 5,
+            EveryVboxLoc => local_base as HalfWord + 6,
+            EveryJobLoc => local_base as HalfWord + 7,
+            EveryCrLoc => local_base as HalfWord + 8,
+            ErrHelpLoc => local_base as HalfWord + 9,
+            ToksBase => local_base as HalfWord + 10,
+            BoxBase => toks_base as HalfWord + 256,
+            CurFontLoc => box_base as HalfWord + 256,
+        }
+    }
+}
+
+const math_font_base: usize = EquivRegion4Loc::CurFontLoc.value() as usize + 1; // table of 48 math font numbers
+const cat_code_base: usize = math_font_base + 48; // table of 256 command codes (the ``catcodes'')
+const lc_code_base: usize = cat_code_base + 256; // table of 256 lowercase mappings
+const uc_code_base: usize = lc_code_base + 256; // table of 256 uppercase mappings
+const sf_code_base: usize = uc_code_base + 256; // table of 256 spacefactor mappings
+const math_code_base: usize = sf_code_base + 256; // table of 256 math mode mappings
+const int_base: usize = math_code_base + 256; // beginning of region 5
+
+// 236
+const int_pars: usize = 55; // total number of integer parameters
+const count_base: usize = int_base + int_pars; // 256 user \count registers
+const del_code_base: usize = count_base + 256; // 256 delimiter code mappings
+const dimen_base: usize = del_code_base + 256; // beginning of region 6
+
+const scaled_base: usize = dimen_base + dimen_pars; // table of 256 user-defined \dimen registers
+
+const eqtb_size: usize = scaled_base + 255; // largest subscript of eqtb
+
+
+
+
+
+
+pub enum History
+{
+    Spotless,
+    WarningIssued,
+    ErrorMessageIssued,
+    FatalErrorStop,
+}
+
+
+impl History {
+    fn value(&self) -> i32 {
+        match self {
+            History::Spotless => 0,
+            History::WarningIssued => 1,
+            History::ErrorMessageIssued => 2,
+            History::FatalErrorStop => 3,
+        }
+    }
+}
+
 
 pub enum TokenList
 {
@@ -129,18 +429,15 @@ impl TokenList {
     }
 }
 
-// 113
-type QuarterWord = u8;
-type HalfWord = u16;
-type TwoChoices = u8;
-type FourChoices = u8;
-
 
 type StrNumber = i32;
 
 pub struct TexState
 {
     ready_already: i32,
+    // 20
+    xord: [u8;256],
+    xchr: [u8;256],
 
     // 76
     deletions_allowed: bool,
@@ -157,18 +454,20 @@ pub struct TexState
     interrupt: i32,
     OK_to_interrupt: bool,
 
-    mem_bot: i32,
-    mem_top: i32,
-    font_base: i32,
-    hash_size: i32,
-    hash_prime: i32,
-    hyph_size: i32,
-
-    xord: [u8;256],
-    xchr: [u8;256],
-
     // 73
     interaction: Interaction,
+
+    // 109
+    glue_ratio: f32,
+
+    // 116
+    mem: Array<MemoryWord>, // the big dynamic storage area
+    lo_mem_max: HalfWord, // the largest location of variable-size memory in use
+    hi_mem_min: HalfWord, // the smallest location of one-word memory in use
+
+    // 117
+    var_used: i32,
+    dyn_used: i32, // how much memory is in use
 
     // 165
     // debug
@@ -178,11 +477,39 @@ pub struct TexState
     was_lo_max: HalfWord,
     was_hi_min: HalfWord,
     panicking: bool,
-    
+
+    // 213
+    nest: [ListStateRecord; TexState::nest_size],
+    nest_ptr: usize, // 0..nestsize
+    max_nest_stack: usize, // 0..nestsize
+    curlist:ListStateRecord,
+    shown_mode: i32, // -203..203
+
+    // 253
+    eqtb: Array<MemoryWord>,
+    xeq_level: Array<QuarterWord>,
+
+    // 980
+    page_tail: HalfWord,
+    page_contents: u8, // 0..2
+    page_max_depth: Scaled,
+    best_page_break: HalfWord,
+    least_page_cost: i32,
+    best_size: Scaled,
+
+    // 982
+    page_so_far: Array<Scaled>, // 0..7
+    last_glue: HalfWord,
+    last_penalty: i32,
+    last_kern: Scaled,
+    insert_penalties: i32,
 }
 
 impl TexState
 {
+    // 2
+    const banner: &'static str = "This is TeX, Version 3.1415926"; // printed when TEX starts
+
     const mem_max: usize = 30000;
     const mem_min: usize = 0;
     const buf_size: i32 = 500;
@@ -194,7 +521,7 @@ impl TexState
     const font_max: i32 = 75;
     const font_mem_size: i32 = 20000;
     const param_size: i32 = 60;
-    const nest_size: i32 = 40;
+    const nest_size: usize = 40;
     const max_strings: i32 = 3000;
     const string_vacancies: i32 = 8000;
     const pool_size: i32 = 32000;
@@ -205,6 +532,7 @@ impl TexState
     const file_name_size: i32 = 40;
     const pool_name: &'static str = "TeXformats:TEX.POOL                     ";
 
+    // 12 (compiler constants)
     const mem_bot: usize = 0;
     const mem_top: usize = 30000;
     const font_base: i32 = 0;
@@ -212,6 +540,7 @@ impl TexState
     const hash_prime: i32 = 1777;
     const hyph_size: i32 = 307;
 
+    // 110
     const min_quarterword: QuarterWord = 0;
     const max_quarterword: QuarterWord = 255;
     const min_halfword: HalfWord = 0;
@@ -220,11 +549,20 @@ impl TexState
     const format_default_length: i32 = 9;
     const format_area_length: i32 = 0;
     const format_ext_length: i32 = 4;
+
+    // 980
+    const inserts_only: i32 = 1;
+    const box_there: i32 = 2;
+
     
     pub fn new() -> TexState
     {
         return TexState {
             ready_already: 0,
+            // 20
+            xord: [0;256],
+            xchr: [0;256],
+
             // 76
             deletions_allowed: false,
             set_box_allowed: false,
@@ -240,16 +578,19 @@ impl TexState
             interrupt: 0,
             OK_to_interrupt: false,
 
-            mem_bot: 0,
-            mem_top: 30000,
-            font_base: 0,
-            hash_size: 2100,
-            hash_prime: 1777,
-            hyph_size: 307,
+            // 109
+            glue_ratio: 0.,
 
-            xord: [0;256],
-            xchr: [0;256],
+            // 116
+            mem: Array::new(TexState::mem_min, TexState::mem_max), // the big dynamic storage area
+            lo_mem_max: 0, // the largest location of variable-size memory in use
+            hi_mem_min: 0, // the smallest location of one-word memory in use
 
+            // 117
+            var_used: 0,
+            dyn_used: 0, // how much memory is in use
+            
+            // 73
             interaction: Interaction::BatchMode,
 
             // 165
@@ -260,6 +601,32 @@ impl TexState
             was_lo_max: 0,
             was_hi_min: 0,
             panicking: false,
+
+            // 213
+            nest: [ListStateRecord::new(); TexState::nest_size],
+            nest_ptr: 0,
+            max_nest_stack: 0,
+            curlist: ListStateRecord::new(),
+            shown_mode: 0,
+
+            // 253
+            eqtb: Array::new(EquivRegion2Code::ActiveBase.value() as usize, eqtb_size),
+            xeq_level: Array::new(int_base, eqtb_size),
+
+            // 980
+            page_tail: 0,
+            page_contents: 0, // 0..2
+            page_max_depth: 0,
+            best_page_break: 0,
+            least_page_cost: 0,
+            best_size: 0,
+
+            // 982
+            page_so_far: Array::new(0, 7), // 0..7
+            last_glue: 0,
+            last_penalty: 0,
+            last_kern: 0,
+            insert_penalties: 0,
         }
     }
 }
@@ -342,6 +709,33 @@ fn initialize(state: &mut TexState) {
         state.was_hi_min = TexState::mem_max as HalfWord;
         state.panicking = false;
     }
+
+    // 215
+    state.nest_ptr = 0;
+    state.max_nest_stack = 0;
+    state.curlist.mode_field = 1;
+    state.curlist.head_field = 29999;
+    state.curlist.tail_field = 29999;
+    state.curlist.aux_field = MemoryWord::new_i32(-65536000);
+    state.curlist.ml_field = 0;
+    state.curlist.pg_field = 0;
+    state.shown_mode = 0;
+
+    // 991
+    state.page_contents = 0;
+    state.page_tail = 29998;
+    state.mem[29998].set_hh_rh(0);
+    
+    state.last_glue = 65535;
+    state.last_penalty = 0;
+    state.last_kern = 0;
+    state.page_so_far[7] = 0;
+    state.page_max_depth = 0;
+
+    // 254
+    for k in 5263..=6106 {
+        state.xeq_level[k] = 1;
+    }
     
     // Initialize table entries
     #[cfg(not(feature = "release"))]
@@ -368,10 +762,9 @@ fn do_final_end(state:&mut TexState) -> i32
 
 
 fn main() {
-    let banner = "This is TeX, Version 3.1415926"; // printed when TEX starts
-    println!("{}", banner);
-
     let mut state = TexState::new();
+
+    println!("{}", TexState::banner);
 
     state.history = History::FatalErrorStop;
 
@@ -416,7 +809,7 @@ fn main() {
         if TexState::max_quarterword - TexState::min_quarterword < 255 { bad = 19; }
 
         //290
-        if TokenList::CsTokenFlag.value() + EquivRegion2Code::UndefinedControlSequence.value() as i32 > TexState::max_halfword
+        if TokenList::CsTokenFlag.value() + EquivRegion2Code::UndefinedControlSequence.value() as i32 > TexState::max_halfword as i32
         {
             bad = 21;
         }
